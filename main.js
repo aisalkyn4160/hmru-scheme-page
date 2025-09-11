f17677802383f0d50136263211a23ca66a186641
aisalkyn4160 <aisalkyn.4160@gmail.com>
2025-09-11 22:28:24 +0700

    last changing

--- main.js
+++ main.js
@@ -35,3 +35,48 @@ if(document.querySelectorAll('.scheme__item')) {
         }
     })
 }
+
+ymaps.ready(init);
+
+function init() {
+    const maps = [];
+    const mapContainers = ['map1', 'map2', 'map3', 'map4'];
+    
+    // Путь к вашей иконке (замените на свой)
+    const iconPath = './public/scheme/mapico.svg';
+    
+    // Координаты для каждой карты (по одному адресу на карту)
+    const addresses = [
+        [55.758215, 37.768453], // Москва, Красная площадь
+        [55.758215, 37.768453], // СПб, Эрмитаж
+        [55.758215, 37.768453], // Казань, Кремль
+    ];
+    
+    mapContainers.forEach((containerId, index) => {
+        const map = new ymaps.Map(containerId, {
+            center: addresses[index], // центр карты = адрес метки
+            zoom: 12 // увеличенный zoom для точного отображения адреса
+        });
+        
+        maps.push(map);
+        
+        // Добавляем ОДНУ метку с кастомной иконкой
+        addSingleMarkerToMap(map, addresses[index], iconPath, `Адрес ${index + 1}`);
+    });
+}
+
+function addSingleMarkerToMap(map, coordinates, iconPath, title) {
+    // Создаем ОДНУ метку с кастомной иконкой
+    const placemark = new ymaps.Placemark(coordinates, {
+        balloonContent: title,
+        hintContent: title
+    }, {
+        // Настройки иконки
+        iconLayout: 'default#image',
+        iconImageHref: iconPath, // путь к вашей иконке
+        iconImageSize: [40, 40], // размер иконки
+        iconImageOffset: [-20, -20] // смещение (центрирование)
+    });
+    
+    map.geoObjects.add(placemark);
+}
